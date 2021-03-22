2526d51a001c3b91f2b4d3bf27e20fafef5c020a
Huỳnh Đức Hoàng <huynh.hoang@rivercrane.com.vn>
2021-03-22 16:09:28 +0700

    update

---
+++ IOS/Util/Utility.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+class Utility{
+    static func config(_ key: String) -> String? {
+        return (Bundle.main.infoDictionary?[key] as? String)?
+            .replacingOccurrences(of: "\\", with: "")
+    }
+}
