4da77fcbfab876abce74c0d9543442d8f5e39b32
Huỳnh Đức Hoàng <huynh.hoang@rivercrane.com.vn>
2021-04-14 11:23:22 +0700

    update

---
+++ IOS/ViewControllers/UpViewController.swift
@@ -0,0 +1,87 @@
+
+import UIKit
+import Photos
+import Alamofire
+
+class UpViewController: BaseViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    lazy var uploadButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Upload", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        
+        button.layer.cornerRadius = 6.0
+        button.layer.borderWidth  = 2
+        button.layer.borderColor  = UIColor.red.cgColor
+        
+        button.addTarget(self, action:#selector(buttonClicked), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var picker: UIImagePickerController = {
+        let picker = UIImagePickerController()
+        picker.sourceType      = .photoLibrary
+        picker.delegate        = self
+        picker.allowsEditing   = false
+        return picker
+    }()
+    
+    @objc func buttonClicked() {
+        switch PHPhotoLibrary.authorizationStatus() {
+            case .notDetermined:
+                PHPhotoLibrary.requestAuthorization { (status) in
+                    if status == .authorized {
+                        DispatchQueue.main.async {
+                            self.present(self.picker, animated: true, completion: nil)
+                        }
+                    }
+                }
+                break
+            
+            case .authorized:
+                self.present(self.picker, animated: true, completion: nil)
+                break
+            
+            case .denied, .restricted: break
+            default: break
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imageUrl = info[.imageURL]{
+            let parameters: Parameters = [
+                "id" : "100"
+            ]
+            
+            AF.upload(
+                multipartFormData: { multipartFormData in
+                    for (key,value) in parameters {
+                        multipartFormData.append((value as! String).data(using: .utf8)!, withName: key)
+                    }
+                    multipartFormData.append(imageUrl as! URL, withName: "car")
+                },
+                to: "http://bizren.fbc.local/upload.php",
+                method: .post
+            )
+            .responseJSON { response in
+                debugPrint(response)
+            }
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    override func layout() {
+        safeView.addSubviews( uploadButton )
+        
+        NSLayoutConstraint.activate([
+            uploadButton.widthAnchor.constraint(equalToConstant: 100),
+            uploadButton.centerXAnchor.constraint(equalTo: safeView.centerXAnchor),
+            uploadButton.centerYAnchor.constraint(equalTo: safeView.centerYAnchor)
+        ])
+    }
+}
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
+# Doc
+~~~
+	https://alamofire.github.io/Alamofire/
+~~~
+
 # Drag & Drop
 ~~~
 	https://xd.adobe.com/view/a500aed2-b5bb-4e75-9c5e-fbd7f01ceeba-f053/screen/56898568-46e6-47e9-afea-751bd7860354
 ~~~
-
-# Upload background
-~~~
-
